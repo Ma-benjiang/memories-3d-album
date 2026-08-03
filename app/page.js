411ab933f9e50661cb83a92d998e8e768acb4dee
Ma-benjiang <65223449+Ma-benjiang@users.noreply.github.com>
2026-08-03 14:05:29 +0800

    Optimize 3D gallery loading

--- app/page.js
+++ app/page.js
@@ -1,5 +1,21 @@
 import StoryHome from "../src/story/story-home";
 
 export default function HomePage() {
-  return <StoryHome />;
+  return (
+    <>
+      <link
+        rel="preload"
+        href="/models/denis-circular-gallery.glb"
+        as="fetch"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/story-woman-cat.webp"
+        as="image"
+        fetchPriority="high"
+      />
+      <StoryHome />
+    </>
+  );
 }
